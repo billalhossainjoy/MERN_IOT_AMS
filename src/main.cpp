c86bd9006e80556aeff3a241addf9957c05153b1
Billal Hossain <billalhossain.bhj@gmail.com>
2025-01-18 14:39:33 +0600

    add fingerprint varification methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <Adafruit_Fingerprint.h>
+#include <WiFi.h>
+
 
 uint8_t id;
 
@@ -166,6 +168,73 @@ public:
 
 		return true;
 	}
+
+	uint8_t getFingerprintID() {
+		uint8_t p = finger.getImage();
+		switch (p) {
+		case FINGERPRINT_OK:
+			Serial.println("Image taken");
+			break;
+		case FINGERPRINT_NOFINGER:
+			Serial.println("No finger detected");
+			return p;
+		case FINGERPRINT_PACKETRECIEVEERR:
+			Serial.println("Communication error");
+			return p;
+		case FINGERPRINT_IMAGEFAIL:
+			Serial.println("Imaging error");
+			return p;
+		default:
+			Serial.println("Unknown error");
+			return p;
+		}
+		// OK success!
+		p = finger.image2Tz();
+		switch (p) {
+		case FINGERPRINT_OK:
+			Serial.println("Image converted");
+			break;
+		case FINGERPRINT_IMAGEMESS:
+			Serial.println("Image too messy");
+			return p;
+		case FINGERPRINT_PACKETRECIEVEERR:
+			Serial.println("Communication error");
+			return p;
+		case FINGERPRINT_FEATUREFAIL:
+			Serial.println("Could not find fingerprint features");
+			return p;
+		case FINGERPRINT_INVALIDIMAGE:
+			Serial.println("Could not find fingerprint features");
+			return p;
+		default:
+			Serial.println("Unknown error");
+			return p;
+		}
+		// OK converted!
+		p = finger.fingerSearch();
+		if (p == FINGERPRINT_OK) {
+			Serial.println("Found a print match!");
+		}
+		else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+			Serial.println("Communication error");
+			return p;
+		}
+
+		else if (p == FINGERPRINT_NOTFOUND) {
+			Serial.println("Did not find a match");
+			return p;
+		}
+		else {
+			Serial.println("Unknown error");
+			return p;
+		}
+
+		// found a match!
+		Serial.print("Found ID #"); Serial.print(finger.fingerID);
+		Serial.print(" with confidence of "); Serial.println(finger.confidence);
+
+		return finger.fingerID;
+	}
 };
 
 Finger finger(Serial2);
@@ -176,6 +245,7 @@ void setup() {
 }
 
 void loop() {
-	
+	finger.getFingerprintID();
+	delay(1000);
 }
 
