95873a07ff20de51971d3525a33101de59068b9d
Billal Hossain <billalhossain.bhj@gmail.com>
2025-01-18 12:50:10 +0600

    setup finger print sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
 #include <Arduino.h>
+#include <Adafruit_Fingerprint.h>
 
+class Finger {
+private:
+	int status;
+	HardwareSerial& mySerial;
+	Adafruit_Fingerprint finger;
+public:
+	Finger(HardwareSerial& serialPort) : mySerial(serialPort), finger(&mySerial) {}
+
+	void begin() {
+		mySerial.begin(57600, SERIAL_8N1, 16, 17);
+		if (finger.verifyPassword()) Serial.println("Fingerprint sensor ready!");
+		else {
+			Serial.println("Fingerprint sensor not found!");
+			while (1);
+		}
+	}
+};
+
+Finger finger(Serial2);
 
 void setup() {
+	Serial.begin(115200);
+	finger.begin();
 }
 
 void loop() {
